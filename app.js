0418fd0267ae1b8717e934619e14f5245c02d9a7
Otto Kafka <ottokafka@yahoo.com>
2017-03-26 19:27:11 +0800

    fix a problem with signing up a new user. The problem was errors was not defined in index.js. The signup new user authentication also works this was just added. Also added authentication to the new user sign up and authentication to signing in a prevous user.

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var mongoose = require("mongoose");
+var session = require("express-session");
+var passport = require("passport");
+var flash = require("connect-flash");
+var validator = require("express-validator");
 
 var index = require('./routes/index');
 
@@ -14,6 +18,7 @@ var app = express();
 
 //             This is the server we connect to and /database name we want
 mongoose.connect("localhost:27017/shopping");
+require("./config/passport");
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
@@ -25,7 +30,12 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
+app.use(session({secret: "gateway", resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
@@ -33,7 +43,7 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new error('Not Found');
   err.status = 404;
   next(err);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require("csurf");
+var passport = require("passport");
 var Product = require("../models/product");
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-//this produces the same data as when used in mongo shell
-    //this function is designed to either get a error or the docs
-
 Product.find(function (err, docs) {
     var productChunks = [];
     //this variable represents the amount of rows = 3 max
@@ -14,9 +16,44 @@ Product.find(function (err, docs) {
     for (var i = 0; i < docs.length; i += chunkSize){
       productChunks.push(docs.slice(i, i + chunkSize));
     }
-    res.render('shop/index', { title: 'Shopping Cart',products: productChunks });
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks});
 });
   });
 
+router.get("/user/signup", function (req, res, next) {
+    var messages = req.flash("error");
+res.render("user/signup", {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0})
+});
+
+router.post("/user/signup", passport.authenticate("local.signup", {
+    successRedirect: "/user/profile",
+    failureRedirect: "/user/signup",
+    failureFlash: true
+}));
+
+router.get("/user/profile", function (req, res, next) {
+    res.render("user/profile");
+});
+
+router.get("/user/signin", function (req, res, next) {
+    var messages = req.flash("error");
+    res.render("user/signin", {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0})
+});
+
+router.post("/user/signin", passport.authenticate("local.signin", {
+    successRedirect: "user/profile",
+    failureRedirect: "user/signin",
+    failureFlash: true
+    }));
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
 
